023e40a9cbdad05d5f634f11b7ebb317749c37a3
Lucia Vazquez Mendoza <47976262+lucyvaz@users.noreply.github.com>
2019-04-21 10:01:19 +0100

    Add files via upload
    Added Profile Page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -26,6 +26,7 @@ import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
+import ProfilePage from '../Profile';
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
@@ -40,13 +41,11 @@ const App = () => (
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route
-        path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
+      <Route path={ROUTES.PROFILE_PAGE} component={ProfilePage} />
       </div>
   </Router>
 );
